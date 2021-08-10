0ce5ac4a52565b41d05ddf83ff0df4f8eb87be04
朝来野study智博 <study_.@users.noreply.github.com>
2021-08-10 23:29:36 +0900

    C言語でデータ型についてのプログラムを修正した(limits・floatヘッダ追加)。
    INT_MAXなどは、
    普通に辞典(付録部分)に載っていた。
    
    以上。

--- c言語/基礎知識用の勉強/1変数勉強/データ型.c
+++ c言語/基礎知識用の勉強/1変数勉強/データ型.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
 int main(void)
 {
-	char char_asakuno = 127;
-	short short_asakuno = 32767;
-	int int_asakuno = 2147483647;
-	long long_asakuno = 2147483647;
-	long long wlong_asakuno = long_asakuno * long_asakuno;
+	char char_asakuno = CHAR_MAX;
+	short short_asakuno = SHRT_MAX;
+	int int_asakuno = INT_MAX;
+	long long_asakuno = LONG_MAX;
+	long long wlong_asakuno = 0;	// これの最大値は？
 
-	float float_asakuno = 3.402823e+038;
-	double double_asakuno = 1.797693e+308;
+	float float_asakuno = FLT_MAX;
+	double double_asakuno = DBL_MAX;
 
 	// bool型はコンパイルエラーが発生した。
 //	bool bool_asakuno = true;	// use of undeclared identifier 'bool'
@@ -17,11 +19,10 @@ int main(void)
 	printf("charサイズ%d\n", char_asakuno);	// charサイズ127
 	printf("shortサイズ%d\n", short_asakuno);	// shortサイズ32767
 	printf("intサイズ%d\n", int_asakuno);	// intサイズ2147483647
-	printf("longサイズ%ld\n", long_asakuno);	// longサイズ2147483647
-	printf("long longサイズ%lld\n", wlong_asakuno);	// long longサイズ4611686014132420609
+	printf("longサイズ%ld\n", long_asakuno);	// longサイズ9223372036854775807
 
-	printf("floatサイズ%f\n", float_asakuno);	// floatサイズ340282306073709652508363335590014353408.000000
-	printf("doubleサイズ%f\n", double_asakuno);	// doubleサイズ179769300000000004979913091153546431177385676945343486730197993498529636492108312404543796370045487218955201046137662191918513706549560771088224207924409275479864981823815660983343422176365744870072127934490865277449576937261468130920376085948653305075071243237207672347403131791038321491100101082182265602048.000000
+	printf("floatサイズ%f\n", float_asakuno);	// floatサイズ340282346638528859811704183484516925440.000000
+	printf("doubleサイズ%f\n", double_asakuno);	// doubleサイズ179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368.000000
 
 //	printf("boolサイズ%s\n", bool_asakuno);	// use of undeclared identifier 'bool_asakuno'; did you mean 'long_asakuno'?
 											// format specifies type 'char *' but the argument has type 'long' [-Wformat]
