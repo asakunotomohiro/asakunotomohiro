c37eac264990042be976c1ed4bd67913cd5faa2a
朝来野study智博 <study_.@users.noreply.github.com>
2021-08-10 23:06:24 +0900

    C言語でextern修飾子についてのプログラムを作成(todo:検証になっていない)。
    やりたいことは、他の関数からも別ファイルの変数を参照できることを検証する。
    しかし、プログラムミスがあり、使えているように見えているが、検証には甘い。
    
    以上。

---
+++ c言語/基礎知識用の勉強/1変数勉強/extern修飾子.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int tomohiro;
+
+// 以下、extern修飾子のテスト用.c
+void func_asakuno( void );
+
+/*
+ * extern修飾子
+ *	他のファイルや他のブロックで定義されているグローバル変数を使えるようにするための修飾子。
+ *	修飾子を使わない場合、内部変数扱いになる(今までの変数宣言だからだよね)。
+ *
+ */
+
+// 以下の場合、同じファイル内であれば普通に使える。
+static int asakuno;
+
+void func_tomohiro( void ){
+//	static int tomohiro = 999;
+
+	printf("asakunotomohiro関数, asakuno：%d\n", asakuno);
+	printf("asakunotomohiro関数, tomohiro：%d\n", tomohiro);
+	asakuno++;
+	tomohiro++;
+}
+
+int main(void)
+{
+	func_asakuno();
+		// asakuno関数, tomohiro：0
+		// asakuno関数, tomohiro：20210810
+	printf("main関数, asakuno：%d\n", asakuno );	// main関数, asakuno：0
+	asakuno = 20210810;
+	printf("main関数, asakuno：%d\n", asakuno );	// main関数, asakuno：20210810
+	func_tomohiro();
+		// asakunotomohiro関数, asakuno：20210810
+		// asakunotomohiro関数, tomohiro：20210810
+}
