f6e089ce3ce82819e8b309ec8baf67e4ff8bbd78
朝来野study智博 <study_.@users.noreply.github.com>
2021-08-23 17:01:19 +0900

    Python言語で基礎知識の関数部分を勉強した。
    ここでも30分以上掛かった。
    しかし、こんなものだと思うことにする。
    「繰り返し」部分と同じ程度の時間なのであれば、30分程度なのが平均なのだろう。
    今更ではあるのだが、他のプログラミング言語を勉強する目安にもなるだろう。
    長くても1時間未満を目標にできる限り最小限の規模に収めること。
    
    以上。

---
+++ Python言語/基礎知識用の勉強/5関数勉強/関数(引数あり-戻り値あり).py
@@ -0,0 +1,7 @@
+def Hello(word):
+    word = word + " World(引数あり-戻り値あり)."
+    return word
+
+# 上記呼び出し方法
+word = Hello("Hello")
+print(word)
---
+++ Python言語/基礎知識用の勉強/5関数勉強/関数(引数無し-戻り値あり).py
@@ -0,0 +1,6 @@
+def Hello():
+    return "Hello World(引数あり)."
+
+# 上記呼び出し方法(引数あり)
+word = Hello()
+print(word)
