009300e2cd636b5217a1112aa27fdeb4816263f0
プログラミングPerl言語の基礎知識勉強_asakunotomohiro <perl_study_asakunotomohiro@users.noreply.github.com>
2021-08-31 16:54:37 +0900

    Perl言語用の基礎知識の勉強(決められた配列部分)完了。
    例えば、コムシス情報システム株式会社の場合、(前にも話したがドコモ開発時にPerl独自の機能を使わず、Perlを使うだけ無駄な使い方を強要してきた)独自ルールがある。
    そのため、配列関係もそのひとつで、必ず添え字を使い、配列内容に取得や代入をしなければならない。
    当たり前のことだが、Perlで他のプログラミング言語のように添え字を使った利用方法では処理速度が遅くなるのは有名な話だ。
    
    https://www.comjo.co.jp/company/message.html
    社長の言葉は、
    > 略）社員一人ひとりが先進的な技術力を絶え間なく向上させるとともに、（略
    だが、
    絶えるよ。
    技術力向上どころか、真逆にプログラミングの性能を劣化させることに執着しており、向上心が全くなく、周りの人を巻き込んでバグを埋め込むことに邁進する。
    だから私の前任者は嫌で嫌でたまらなくなって逃げ出したというのに、、、
    私は派遣社員だったため、バグを埋め込めと言われれば躊躇せずに埋め込むけど。
    
    ということで、私は今や自由だ。
    派遣社員から解放されたどころか、社会で働くことからも解放された身・・・悲しいかな。
    努力しているはずなのに、なぜか報われない。
    早く働きたいのだが、書類選考で落とされることが多い。
    それでもプログラミングの勉強は怠けずに精進するつもりだ。
    であれば、向上心を見せ、Perl独自の配列操作をすることが責務だと考え、Perlだけは添え字を使わずに配列を操作することにする。
    
    以上。

---
+++ Perl言語/基礎知識用の勉強/2配列勉強/リスト.pl
@@ -0,0 +1,45 @@
+use v5.24;
+
+my @asakuno = (1..10);
+
+# 上記の格納方法では、1から1づつ増加し、10まで格納される。
+say "@asakuno";	# 1 2 3 4 5 6 7 8 9 10
+
+@asakuno = (1.7..5.7);	# 小数点部分が切り捨てられる。
+say "@asakuno";	# 1 2 3 4 5
+
+@asakuno = (5..1);	# 増加する使い方のみできるため、空リストが生成される。
+say "@asakuno";	# 空
+
+my $number10 = 10;
+my $number20 = 20;
+@asakuno = ($number10..$number20);
+say "@asakuno";	# 10 11 12 13 14 15 16 17 18 19 20
+
+# 以下、文字の扱い。
+my $kakko = ")(";
+say $kakko;
+my @string = qw! asa kuno tomo hiro !;
+say "@string";	# asa kuno tomo hiro
+
+#@string = qw# @string $kakko #;
+@string = ( @string, $kakko );
+say "@string";	# asa kuno tomo hiro )(
+
+my @string = qw! asa kuno tomo hiro !;
+say "@string";	# asa kuno tomo hiro
+@asakuno = (@string, undef, $kakko);
+say "@asakuno";	# asa kuno tomo hiro  )(
+	# 意図的に"undef"を入れている(これ以外の代入方法はないはず)。
+
+say "-" x 30;
+@string = ();
+@asakuno = (1..5);	# 増加する使い方のみできるため、空リストが生成される。
+say "@string, @asakuno";	# , 1 2 3 4 5
+@string = @asakuno;
+$asakuno[0] = 99;
+say "@string, @asakuno";	# 1 2 3 4 5, 99 2 3 4 5
+	# 配列データはコピーされる(参照ではない)。
+
+
+# 以上。
