2e99bbf70e019d6455c3b4dec8b43c1fda86f6de
プログラミングPerl言語応用知識_asakunotomohiro <perl_ObjectOrientedProgrammingChapterTwoStudy_asakunotomohiro@users.noreply.github.com>
2021-11-17 23:38:12 +0900

    Perl言語での配列の一般利用方法について追記(オブジェクト指向Perlマスターコース-2章)。

--- Perl言語/基礎知識用の勉強/2配列勉強/配列.pl
+++ Perl言語/基礎知識用の勉強/2配列勉強/配列.pl
@@ -15,13 +15,27 @@ say "-" x 30;
 
 # 最後の要素取り出し。
 say $asakuno[-1];	# 20210901
+say $asakuno[-2];	# 20210831	←☆最後から2番目の要素。
 
+my $count_asakuno = @asakuno;
+say $count_asakuno;	# 2
+
+say "-" x 30;
 # 計算結果の代入。
+say "以下、計算結果の代入。";
 $asakuno[9] = $asakuno[1] + 9;
+say $asakuno[8];	# 空
+say $asakuno[9];	# 20210910
+
+say "-" x 30;
 
 # 最後の要素取り出し。
 say $asakuno[-1];	# 20210910
 say $#asakuno;	# 9
+say $asakuno[$#asakuno];	# 20210910
+say $asakuno[10];	# 空undef
+
+say "-" x 30;
 
 
 # 1つづつ取り出し。
@@ -106,6 +120,8 @@ else {
 my $tomohiro = 1.1234;
 say $asakuno[$tomohiro];	# 20210901
 #	これは、小数点数が切り捨てられて添え字として利用されているため、2つ目の要素(添え字1)が取り出された。
+say $asakuno[$#asakuno];	# 20210901
+say $#asakuno;	# 1
 
 say "以上。"
 # vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=perl noexpandtab:
