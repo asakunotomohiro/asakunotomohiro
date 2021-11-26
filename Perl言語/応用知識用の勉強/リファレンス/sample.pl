edc5e865d9b5eb51f3c54b286c8cc6903a6e2f2d
プログラミングPerl言語応用知識_asakunotomohiro <perl_ObjectOrientedProgrammingChapterTwoStudy_asakunotomohiro@users.noreply.github.com>
2021-11-27 00:22:12 +0900

    Perl言語でのリファレンスのアロー演算子について追記(オブジェクト指向Perlマスターコース-2章)。

--- Perl言語/応用知識用の勉強/リファレンス/sample.pl
+++ Perl言語/応用知識用の勉強/リファレンス/sample.pl
@@ -10,11 +10,9 @@ sub refFunc(){
 }
 
 sub sample() {
-my $foo = 20211126;
-my @ARGV = (20211126, 20211127);
-my %ENV = (20211126=>"朝来野智博", 20211127=>"asakunotomohiro");
-
-
+	my $foo  = 20211126;
+	my @ARGV = (20211126, 20211127);
+	my %ENV  = (20211126=>"朝来野智博", 20211127=>"asakunotomohiro");
 
 	my $scalarref = \$foo;     # 配列
 	my $arrayref  = \@ARGV;    # 配列
@@ -38,6 +36,22 @@ my %ENV = (20211126=>"朝来野智博", 20211127=>"asakunotomohiro");
 	say ${$hashref}{20211127};	# asakunotomohiro
 	say &$coderef;				# ハンドラー関数
 	# 1	←☆これ何？
+
+	say "-" x 30;
+
+# 以下、配列リファレンスをスカラーリファレント扱いしたため、エラーになった。
+#	say $$arrayref;	Not a SCALAR reference at sample.pl line 42.
+
+	say "-" x 30;
+	say "以下、アロー演算子";
+	# 配列。
+	say $arrayref->[0];		# 20211126
+	say $arrayref->[1];		# 20211127
+	# ハッシュ
+	say $hashref->{20211126};	# 朝来野智博
+	say $hashref->{20211127};	# asakunotomohiro
+	say $coderef->();			# ハンドラー関数
+								# 1
 }
 &sample("asakuno");
 
