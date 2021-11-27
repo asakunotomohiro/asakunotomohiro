3dda0459957059c86470069954bb28896aedf4e0
プログラミングPerl言語アルゴリズムChapter3_asakunotomohiro <perl_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2021-11-27 17:05:19 +0900

    Perl言語での「Pythonで学ぶアルゴリズムの教科書」の「Chapter3：キュー」完了。

--- Perl言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter3_データ構造を学ぶ/2.キュー/queue.pl
+++ Perl言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter3_データ構造を学ぶ/2.キュー/queue.pl
@@ -9,7 +9,10 @@ sub enqueue() {
 	# データ投入。
 	my ( $ii, $enqueue) = @_;
 
-	unshift @$enqueue, $ii;
+	# 以下、末尾に追加する。
+	push @$enqueue, $ii;
+# 以下、先頭に追加する。
+#	unshift @$enqueue, $ii;
 }
 
 sub dequeue() {
@@ -20,6 +23,7 @@ sub dequeue() {
 		return "取り出すデータが存在しません。";
 	}
 	else {
+		# 以下、先頭から取り出す。
 		return shift @$dequeue;
 	}
 }
@@ -28,30 +32,30 @@ sub main() {
 	my @queue;
 
 	for my $ii ( 0..3 ) {
-		&enqueue($ii, \@queue);
+		&enqueue($ii, \@queue);		# 先に入れている。
 	}
-	&enqueue("asakuno", \@queue);
+	&enqueue("asakuno", \@queue);	# 途中に追加。
 	for my $ii ( 4..5 ) {
 		&enqueue($ii, \@queue);
 	}
-	&enqueue("tomohiro", \@queue);
+	&enqueue("tomohiro", \@queue);	# 最後に追加。
 #	for my $ii ( 0..8 ) {
 #		my $data = &dequeue(\@queue);
 #		say "取り出したデータ：", $data;
 #	}
-		say &dequeue(\@queue);	# tomohiro
-		say &dequeue(\@queue);	# 5
+		say &dequeue(\@queue);	# 0
+		say &dequeue(\@queue);	# 1
 }
 &main("asakuno");
 # 出力結果：
-#			取り出したデータ：tomohiro
-#			取り出したデータ：5
-#			取り出したデータ：4
-#			取り出したデータ：asakuno
-#			取り出したデータ：3
-#			取り出したデータ：2
-#			取り出したデータ：1
 #			取り出したデータ：0
+#			取り出したデータ：1
+#			取り出したデータ：2
+#			取り出したデータ：3
+#			取り出したデータ：asakuno
+#			取り出したデータ：4
+#			取り出したデータ：5
+#			取り出したデータ：tomohiro
 #			取り出したデータ：取り出すデータが存在しません。
 
 
