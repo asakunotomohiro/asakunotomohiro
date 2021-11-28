cee20c305a9e9aa8a1361ba83ff6bbb892f27532
プログラミングJavaScript言語の構造化プログラミング用環境構築_asakunotomohiro <javaScript_StructuredPogramming_studyEnvironment_asakunotomohiro@users.noreply.github.com>
2021-11-28 15:31:53 +0900

    JavaScript言語で基礎知識の変数部分の前段階として、リテラルとエスケープシーケンスを勉強した。

--- JavaScript言語/基礎知識用の勉強/1変数勉強/実行済み/Variable.js
+++ JavaScript言語/基礎知識用の勉強/1変数勉強/実行済み/Variable.js
@@ -1,4 +1,29 @@
 // ダイアログ出力。
-console.log("Hello World!");
+console.log("基礎知識5種類の勉強のうち、変数。");
+
+/*
+ * 以下、リテラル。
+ */
+// 以下、文字列リテラル。
+console.log("123");
+// 以下、整数リテラル。
+console.log(456);
+// 以下、小数リテラル。
+console.log(789.1);
+
+/*
+ * 以下、エスケープシーケンス。
+ */
+console.log("ハローワールドからの\"こんにちは世界。\"");
+console.log('ハローワールドからの"こんにちは世界。"');
+console.log('ハローワールド。\nそして、\rこんにちは世界。');
+// 他のエスケープシーケンス
+//		\'
+//		\"
+//		\\
+//		\t
+//		\n	←☆Unix系のLF。
+//		\r	←☆これは、いつ使う？どう使う？	WindowsのCRLF。
+
 
 /* vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=javascript noexpandtab: */
