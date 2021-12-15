7ec05754a9208fcd2f1997ee90b93a3ab520b89c
プログラミングPerl言語_配列リファレンスの勉強_asakunotomohiro <perlProgramming_basicKnowledgeStudy_ArrayReference@users.noreply.github.com>
2021-12-15 22:44:26 +0900

    Perl言語でのオブジェクト指向用の入門部分として、配列リファレンスの勉強(無名配列リファレンスプログラム追記)。

--- Perl言語/応用知識用の勉強/リファレンス/配列のリファレンス.pl
+++ Perl言語/応用知識用の勉強/リファレンス/配列のリファレンス.pl
@@ -2,8 +2,28 @@ use v5.24;
 
 my $asakuno = "朝来野智博";
 my @asakuno = ("朝来野", "智博");
+my $tomohiro = \@asakuno;
+say "$tomohiro";		# ARRAY(0x7fa802005388)
+say "@$tomohiro";		# 朝来野 智博
+say "$$tomohiro[0]";	# 朝来野
+say "$$tomohiro[1]";	# 智博
 
-say "配列リファレンス";
+say "以下、無名配列リファレンス失敗";
+my $tomohiro = \("朝来野", "智博");
+say "$tomohiro";		# SCALAR(0x7fa2bb804928)
+						# 要は、(\"朝来野", \"智博")と同じ事になっているため、配列とは無縁状態。
+#say "@$tomohiro";		# 
+#say "$$tomohiro[0]";	# 
+#say "$$tomohiro[1]";	# 
+
+say "以下、無名配列リファレンス";
+my $tomohiro = ["朝来野", "智博"];
+say "$tomohiro";		# ARRAY(0x7ff649003610)
+say "@$tomohiro";		# 朝来野 智博
+say "$$tomohiro[0]";	# 朝来野
+say "$$tomohiro[1]";	# 智博
+
+say "以下、配列リファレンス";
 
 sub arrayReference() {
 	my $one = shift @_;
