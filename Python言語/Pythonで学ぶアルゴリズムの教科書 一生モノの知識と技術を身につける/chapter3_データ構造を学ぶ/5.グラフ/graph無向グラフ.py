8af85222625a732f917142c5f895230bfe554844
プログラミングPython言語アルゴリズムChapter3勉強_asakunotomohiro <python_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2022-01-02 14:38:41 +0900

    Python言語での「Pythonで学ぶアルゴリズムの教科書」の「Chapter3：グラフ」無向グラフ完了。

---
+++ Python言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter3_データ構造を学ぶ/5.グラフ/graph無向グラフ.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+#   これを付けなければ、エラーが発生する(Python2だと判断された)。
+print("朝来野智博")
+print("リスト")
+
+print("グラフ")
+
+
+# 以下、プログラム
+def graph(data, node):
+    for yy in range(5):
+        for xx in range(yy, 5):
+            if data[yy][xx] == 1 and data[xx][yy] == 1:
+                print(node[yy]+"<--->" + node[xx])
+
+
+def main():
+    data = [    # グラフの定義。
+            [0, 1, 1, 0, 0, ],
+            [1, 0, 1, 1, 0, ],
+            [1, 1, 0, 0, 1, ],
+            [0, 1, 0, 0, 1, ],
+            [0, 0, 1, 1, 0, ],
+        ]
+    node = [
+            "(0)", "(1)", "(2)", "(3)", "(4)",
+        ]
+    graph(data, node)
+
+
+main()
+# 出力結果：
+#           (0)<--->(1)
+#           (0)<--->(2)
+#           (1)<--->(2)
+#           (1)<--->(3)
+#           (2)<--->(4)
+#           (3)<--->(4)
+
+
+print("以上。")
+# 未だにPython2として認識される。
+# vim: set requires python 3.9.7:
+# vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=python expandtab:
