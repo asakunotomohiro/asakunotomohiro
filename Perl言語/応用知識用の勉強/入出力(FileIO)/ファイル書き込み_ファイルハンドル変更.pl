56eee10c8e261cf77a7d4460a2a604b301c70080
プログラミングPerl言語_入力と出力の勉強_asakunotomohiro <perlProgramming_InputOutputStudy@users.noreply.github.com>
2022-01-03 16:05:43 +0900

    Perl言語の応用知識用(ファイル操作)ファイルハンドルからのファイル書き込み(標準出力先の変更その1)。

---
+++ Perl言語/応用知識用の勉強/入出力(FileIO)/ファイル書き込み_ファイルハンドル変更.pl
@@ -0,0 +1,23 @@
+package perlStudyInputOutput;
+$VERSION = "0.002";
+use v5.24;
+
+my $asakuno = "朝来野智博";
+my @asakuno = qw( 朝来野 智博 朝来野智博 );
+
+say "ファイル書き込み。";
+
+sub asakunoInputOutput() {
+	if( ! open FILE, '>>', $_[0]) {
+		die "書き込めるファイルを引数に渡すこと($!)。"
+	}
+	select FILE;
+	foreach( @asakuno ) {
+		say $_;
+	}
+	select STDOUT;
+}
+&asakunoInputOutput(@ARGV);
+
+say "以上。"
+# vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=perl noexpandtab:
