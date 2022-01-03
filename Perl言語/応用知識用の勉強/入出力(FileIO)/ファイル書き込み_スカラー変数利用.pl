e567290d5017c34b6bf67db116ea7088dd38e95c
プログラミングPerl言語_入力と出力の勉強_asakunotomohiro <perlProgramming_InputOutputStudy@users.noreply.github.com>
2022-01-03 17:21:07 +0900

    Perl言語の応用知識用(ファイル操作)ファイルハンドル(スカラー変数での出力用プログラム変更)。

--- Perl言語/応用知識用の勉強/入出力(FileIO)/ファイル書き込み_スカラー変数利用.pl
+++ Perl言語/応用知識用の勉強/入出力(FileIO)/ファイル書き込み_スカラー変数利用.pl
@@ -11,7 +11,15 @@ sub asakunoInputOutput() {
 	open my $file_fh, '>>', $_[0]
 		or die "$_[0]のファイルオープン失敗($!)";
 	foreach( @asakuno ) {
-		say $file_fh $_;
+		say $file_fh $_;	# `say STDOUT;`ならば利用可能。
+		# say $file_fh;#	←☆これを用いた場合、以下のGLOBとやらが出力される(ファイル書き込みはしない・標準出力)。
+			# GLOB(0x7ff35f001988)
+			# GLOB(0x7ff35f001988)
+			# GLOB(0x7ff35f001988)
+		# say {$file_fh} $_;	←☆これなら問題なくファイル書き込みできる。
+		# say {$file_fh};	←☆エラー。
+			# syntax error at ファイル書き込み_スカラー変数利用.pl line 20, near "};"
+			# Execution of ファイル書き込み_スカラー変数利用.pl aborted due to compilation errors.
 	}
 }
 &asakunoInputOutput(@ARGV);
