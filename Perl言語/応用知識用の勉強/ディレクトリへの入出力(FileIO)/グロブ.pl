f5374b22d40ae819673f98a0a29b308511fb49b4
プログラミングPerl言語_ディレクトリへの入力と出力の勉強_asakunotomohiro <perlProgramming_directoryInputOutputStudy@users.noreply.github.com>
2022-01-08 16:30:34 +0900

    Perl言語の応用知識用(ファイル操作)Perlのレガシー表記グロブについて追記。

--- Perl言語/応用知識用の勉強/ディレクトリへの入出力(FileIO)/グロブ.pl
+++ Perl言語/応用知識用の勉強/ディレクトリへの入出力(FileIO)/グロブ.pl
@@ -29,5 +29,34 @@ sub asakunoInputOutput() {
 }
 &asakunoInputOutput();
 
+sub asakunoGlob() {
+	say "ここの関数は、グロブの別表記用。";
+	my @plfile = <*.pl>;
+	say "@plfile";	# helloWorld.pl version.pl
+
+	@plfile = glob <*.pl *.md>;	# この書き方は間違い。
+	say "@plfile";	# helloWorld.pl
+
+	@plfile = <*.pl *.md>;
+	say "@plfile";	# helloWorld.pl version.pl 環境構築(インストール).md README.md
+
+	@plfile = <*>;
+	say "@plfile";	# 基礎知識用の勉強 応用知識用の勉強 環境構築(インストール).md helloWorld.pl Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける README.md version.pl
+
+	my $period = '.*';
+	@plfile = <$period>;	# この書き方は間違い(ファイルハンドル扱いになっている)。
+	say "@plfile";	# 空文字列(undef)
+
+	my $period = '.';
+	@plfile = <$period/.*>;
+	say "@plfile";	# . .. .DS_Store .README.md.swp
+
+	$currentDir = getcwd();
+	@plfile = <$currentDir/.*>;
+	say "@plfile";	# /Users/asakunotomohiro/study勉強用Githubリポジトリ/Perl言語/. /Users/asakunotomohiro/study勉強用Githubリポジトリ/Perl言語/.. /Users/asakunotomohiro/study勉強用Githubリポジトリ/Perl言語/.DS_Store /Users/asakunotomohiro/study勉強用Githubリポジトリ/Perl言語/.README.md.swp
+	say "関数終了。"
+}
+&asakunoGlob();
+
 say "以上。"
 # vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=perl noexpandtab:
