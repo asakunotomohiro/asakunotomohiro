03e6e8d9a40e7e2b703c872d1bef76d8531d3c8b
プログラミングJavaScript言語の構造化プログラミング用環境構築_asakunotomohiro <javaScript_StructuredPogramming_studyEnvironment_asakunotomohiro@users.noreply.github.com>
2022-01-29 12:26:16 +0900

    JavaScript言語の配列について勉強(配列宣言を追記)。
    前回から時間が経っているため、ブラウザでの確認方法を忘れてしまったよ。
    情けない。
    
    以上。

---
+++ JavaScript言語/基礎知識用の勉強/2配列勉強/Array.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+// ダイアログ出力。
+console.log("基礎知識5種類の勉強のうち、配列。");
+
+/*
+ * 以下、配列宣言。
+ */
+let asakuno = [20220129, 20220130, 20220131, 20220132, 20220133, 20220134, ];
+
+
+console.log("配列要素1番目：" + asakuno[0]);
+console.log("配列要素2番目：" + asakuno[1]);
+console.log("配列要素3番目：" + asakuno[2]);
+console.log("配列要素4番目：" + asakuno[3]);
+console.log("配列要素5番目：" + asakuno[4]);
+console.log("配列要素6番目：" + asakuno[5]);
+console.log("配列要素7番目：" + asakuno[6]);
+// 出力結果。
+//			配列要素1番目：20220129
+//			配列要素2番目：20220130
+//			配列要素3番目：20220131
+//			配列要素4番目：20220132
+//			配列要素5番目：20220133
+//			配列要素6番目：20220134
+//			配列要素7番目：undefined
+
+
+/* vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=javascript noexpandtab: */
