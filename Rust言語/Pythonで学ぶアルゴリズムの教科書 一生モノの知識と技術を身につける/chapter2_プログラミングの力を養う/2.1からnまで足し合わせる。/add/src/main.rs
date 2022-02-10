c4e72eca964c4a49bdc55160f208c672d6b61c5f
プログラミングRust言語アルゴリズムChapter2_asakunotomohiro <Rust_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2022-02-10 17:01:29 +0900

    Rust言語での「Pythonで学ぶアルゴリズムの教科書-Chapter2：1からnまで足し合わせる」工夫した加算プログラムについて追記。

--- Rust言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/2.1からnまで足し合わせる。/add/src/main.rs
+++ Rust言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/2.1からnまで足し合わせる。/add/src/main.rs
@@ -6,20 +6,30 @@
 //	println!("合計点 {0}", count);	// 合計点 55
 //}
 
-fn add2( total: i32 ) -> i32 {
-	let mut count = 0;
-	let max = total + 1;
-	for ii in 1..max {
-		count += ii;
-	}
-	//println!("合計点 {0}", count);	// 合計点 55
-	count
+//fn add2( total: i32 ) -> i32 {
+//	let mut count = 0;
+//	let max = total + 1;
+//	for ii in 1..max {
+//		count += ii;
+//	}
+//	//println!("合計点 {0}", count);	// 合計点 55
+//	count
+//}
+
+fn add3( total: i32 ) -> i32 {
+	//let max = total + 1;
+	//let sumtotal = (1+max) * max / 2;
+	let sumtotal = (1+total) * total / 2;
+
+	return sumtotal;
 }
 
 fn main() {
 	//add1();
-	let total = add2(10);
-	println!("合計点 {0} << add2関数", total);	// 合計点 55 << add2関数
+	//let total = add2(10);
+	//println!("合計点 {0} << add2関数", total);	// 合計点 55 << add2関数
+	let total = add3(10);
+	println!("合計点 {0} << add3関数", total);	// 合計点 55 << add3関数
 }
 
 /*	vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=rust noexpandtab: */
