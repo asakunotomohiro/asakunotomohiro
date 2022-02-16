2b9f8c39683df4db84391152e94a374b24235eaf
プログラミングC#言語アルゴリズムChapter2_asakunotomohiro <C#_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2022-02-16 22:01:51 +0900

    C#言語での「Pythonで学ぶアルゴリズムの教科書-Chapter2：九九の式を出力する」四角形っぽい表示に置き換えたプログラム追記。

--- Cシャープ言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/3.九九の式を出力する。/kuku/kuku/Program.cs
+++ Cシャープ言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/3.九九の式を出力する。/kuku/kuku/Program.cs
@@ -4,7 +4,7 @@ namespace sample
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
+		public static void kuku1()
 		{
 			for (var ii = 1; 10 > ii; ii++)
 			{
@@ -13,6 +13,22 @@ namespace sample
 					Console.WriteLine("{0}x{1}={2,2:G}", ii, jj, ii * jj);
 				}
 			}
+		}
+		public static void kuku2()
+		{
+			for (var ii = 1; 10 > ii; ii++)
+			{
+				for (var jj = 1; 10 > jj; jj++)
+				{
+					Console.Write("{0,2:D}x{1:D}={2,2:G}", jj, ii, ii * jj);
+				}
+				Console.WriteLine("");
+			}
+		}
+		public static void Main(string[] args)
+		{
+			//kuku1();
+			kuku2();
 		}
 	}
 }
