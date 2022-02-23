e8973c2a773a8bcb69a5d64d29aea09d04ca0533
プログラミングC言語アルゴリズム勉強_asakunotomohiro <cLang_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2022-02-23 18:17:21 +0900

    C言語での「Pythonで学ぶアルゴリズムの教科書」の「Chapter2：nの階乗を求める」再帰用プログラム例追記。
    久々に触ったらやり方を忘れていた。
    あぁC言語案件で就職活動しているが、無謀かな・・・。
    
    以上。

--- c言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/5.nの階乗を求める。/factorial.c
+++ c言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/5.nの階乗を求める。/factorial.c
@@ -12,11 +12,37 @@ int factorial_sample()
 	return 0;
 }
 
-int main(void)
+int recursion_sample(int count)
 {
-	factorial_sample();	// 10!：3628800
+	if ( count > 0 ) {
+		recursion_sample( count - 1 );
+	}
+	printf("再帰関数%d回目の実行\n", count );
 
 	return 0;
 }
+
+int main(void)
+{
+	//factorial_sample();	// 10!：3628800
+	recursion_sample(10);	// 再帰
+
+	return 0;
+}
+/*
+ * 出力結果。
+ *
+			再帰関数0回目の実行
+			再帰関数1回目の実行
+			再帰関数2回目の実行
+			再帰関数3回目の実行
+			再帰関数4回目の実行
+			再帰関数5回目の実行
+			再帰関数6回目の実行
+			再帰関数7回目の実行
+			再帰関数8回目の実行
+			再帰関数9回目の実行
+			再帰関数10回目の実行
+ */
 
 // vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=c noexpandtab:
