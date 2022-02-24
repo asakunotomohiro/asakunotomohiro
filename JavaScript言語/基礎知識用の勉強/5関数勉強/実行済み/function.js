aa15ee4b43818cbb822ff48e5d5727f61f776b54
プログラミングJavaScript言語の構造化プログラミング用環境構築_asakunotomohiro <javaScript_StructuredPogramming_studyEnvironment_asakunotomohiro@users.noreply.github.com>
2022-02-24 15:09:37 +0900

    JavaScript言語用の基礎知識の勉強(関数-引数・戻り値の組み合わせ)引数なしの戻り値ありのプログラム追記。

--- JavaScript言語/基礎知識用の勉強/5関数勉強/実行済み/function.js
+++ JavaScript言語/基礎知識用の勉強/5関数勉強/実行済み/function.js
@@ -22,6 +22,16 @@ function insay(arg) {
 	// 引数あり-戻り値無し。
 	console.log("関数内から出力(" + arg + ")。");
 }
-insay('引数');
+//insay('引数');
+
+function sayret() {
+	// 引数なし-戻り値あり。
+	let func = 20220224;
+	console.log("関数内から出力。");	// 関数内から出力。
+
+	return func;
+}
+let ret = sayret();
+console.log("戻り値出力(" + ret + ")。");	// 戻り値出力(20220224)。
 
 /* vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=javascript noexpandtab: */
