119fd01fc1ce5e15ec1504c07145770ca6c13e32
プログラミングRust言語アルゴリズムChapter2_asakunotomohiro <Rust_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2022-02-25 00:27:34 +0900

    Rust言語での「Pythonで学ぶアルゴリズムの教科書-Chapter2：九九の式を出力する」縦に並ぶプログラム追記。

--- Rust言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/3.九九の式を出力する。/kuku/src/main.rs
+++ Rust言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/3.九九の式を出力する。/kuku/src/main.rs
@@ -8,7 +8,7 @@
 	呼び出さない関数は定義できないようだ・・・消すのが面倒くさいぞ。
 */
 
-fn samplewhile() {
+/*fn samplewhile() {
 	let mut ii = 1;
 	while ii < 3 {
 		let mut jj = 1;
@@ -19,10 +19,20 @@ fn samplewhile() {
 		ii += 1;
 	}
 }
+*/
+
+fn kuku1() {
+	for ii in 1..10 {
+		for jj in 1..10 {
+			println!("{0}x{1}={2:>2}", ii, jj, ii*jj);
+		}
+	}
+}
 
 fn main() {
 	//sampleforeach();
-	samplewhile();
+	//samplewhile();
+	kuku1();
 }
 
 /*	vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=rust noexpandtab: */
