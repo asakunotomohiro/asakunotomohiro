036421de4b62927976c27d3b6e8b423c683f9e18
プログラミングJava言語用アルゴリズムの勉強_asakunotomohiro <java_algorithmStudy_asakunotomohiro@users.noreply.github.com>
2022-02-25 09:36:48 +0900

    Java言語での「Pythonで学ぶアルゴリズムの教科書」の「Chapter2：素数を求める」プログラム追記。
    Goと同じにしたはずが、抜けがあった。
    どんなメクラなのか・・・困った。
    こんなことだから仕事が出来ず、情けない思いをする。
    あぁ。
    働かずに金が入ってこないかな・・・その仕組みを作るには、働いておく必要があるわけで、、、
    
    会社を自分で設立したいが、それはそれで無理なんだよね。
    面倒くさい以上に、やりたいことがない。
    自営業は無理。
    以上。

--- Java言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/4.素数を求める。/prime.java
+++ Java言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/4.素数を求める。/prime.java
@@ -1,17 +1,23 @@
 class prime {
 	static void prime(){
 		for( int ii = 2; 101 > ii; ii++ ){
-			var half = ii / 2;	// 商を求める。
-			var flag = true;
-			for( int jj = 2; 101 > jj; jj++ ){
+			int half = ii / 2;	// 商を求める。
+			boolean flag = true;
+			for( int jj = 2; half >= jj; jj++ ){
 				if(( ii % jj ) == 0 ){
 					// 剰余算にて、割り切れるならば、出力対象外にする。
 					flag = false;
 					break;
 				}
+//System.out.printf("%d %% %d = %d\n", ii, jj, ii%jj);	// 0の余り以外がここで表示される(意味の無い確認)。
+//				if( ii == jj ) {
+//					System.out.printf("%d", ii);
+//				}
 			}
-			if( flag == true ) {
-				System.out.printf("%d", ii);
+//System.out.printf("%s\n", flag);
+			//if( flag == true ) {	←☆Bool型故に、単発存在だけで比較が行われる(正確には比較と言わないだろうが)。
+			if( flag ) {
+				System.out.printf("%d,", ii);
 			}
 		}
 	}
