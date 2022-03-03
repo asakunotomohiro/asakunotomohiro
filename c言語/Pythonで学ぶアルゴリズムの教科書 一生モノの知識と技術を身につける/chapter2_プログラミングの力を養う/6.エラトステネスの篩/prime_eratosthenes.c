9359d1b21122bbbad2b6700fcb882f2111a08f61
プログラミングC言語アルゴリズム勉強_asakunotomohiro <cLang_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2022-03-03 19:57:14 +0900

    C言語での「Pythonで学ぶアルゴリズムの教科書」の「Chapter2：エラトステネスの篩」プログラム追記。
    これ作るだけで3時間近く掛かった。
    プログラミングの採用がない。
    しかし、プログラミングが出来ないわけではない。
    プログラムを組めるのだから問題ないとしよう。
    
    以上。

--- c言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/6.エラトステネスの篩/prime_eratosthenes.c
+++ c言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/6.エラトステネスの篩/prime_eratosthenes.c
@@ -2,6 +2,7 @@
 
 int sievingTable(int (*table)[10])
 {
+	// ギブアップ関数。
 	// 篩いテーブル作成。
 	//int table[10][10];
 	for( int ii = 0, jj = 0, kk = 0; 100 > ii; ii++, kk++ ) {
@@ -25,8 +26,19 @@ int sievingTable(int (*table)[10])
 	return 0;
 }
 
+int sievingTableRe(int *table)
+{
+	// 篩いテーブル作成。
+	for( int ii = 0; 100 > ii; ii++ ) {
+		table[ii] = ii;
+	}
+
+	return 0;
+}
+
 int tableView(int (*table)[10])
 {
+	// ギブアップ関数。
 	// 篩いテーブル内容表示。
 	for( int ii=0; 10>ii; ii++ ) {
 		for( int jj=0; 10>jj; jj++ ) {
@@ -38,20 +50,120 @@ int tableView(int (*table)[10])
 	return 0;
 }
 
-int eratosthenes_sieve()
+int tableViewRe(int *table)
 {
+	// 篩いテーブル内容表示。
+	for( int ii=0, jj=0; 100>ii; ii++, jj++ ) {
+		if( jj == 10 ) {
+			printf("\n");
+			jj=0;
+		}
+		if( table[ii] == 999 ) {
+			printf("  /");
+		}
+		else{
+			printf("%3d", table[ii]);
+		}
+	}
+
+	return 0;
+}
+
+int eratosthenes_sieve(int (*table)[10])
+{
+	// ギブアップ関数。
+	// エラトステネスの篩を表示する関数。
+	for( int ii=0; 10>ii; ii++ ) {
+		for( int jj=0; 10>jj; jj++ ) {
+			if( table[ii][jj] != 999 ) {
+				printf("%3d", table[ii][jj]);
+			}
+			else{
+				printf("  /");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+int eratosthenesSieveRe(int *table)
+{
+	// エラトステネスの篩を表示する関数。
+	for( int ii=0, jj=0; 100>ii; ii++, jj++ ) {
+		if( jj == 10 ) {
+			printf("\n");
+			jj=0;
+		}
+		if( table[ii] == 999 ) {
+			printf("  /");
+		}
+		else{
+			printf("%3d", table[ii]);
+		}
+	}
+
+	return 0;
+}
+
+int eratosthenesSieveProcess(int (*table)[10], int start)
+{
+	// ギブアップ関数。
+	// エラトステネスの篩を処理する関数
+	printf("%dの倍数をふるい落とす。\n", start);
+	for( int ii=0; 10>ii; ii++ ) {
+	//for( start=start+start; 10>start; start++ ) {
+		//for( int jj=0; 10>jj; jj++ ) {
+		for( int jj=start+start; 10>jj; jj+=start ) {
+			table[ii][jj] = 999;
+		}
+	}
+
+	return start;
+}
+
+int eratosthenesSieveProcessRe(int *table, int start)
+{
+	// エラトステネスの篩を処理する関数
+	printf("%dの倍数をふるい落とす。\n", start);
+	for( int ii=start+start; 100>ii; ii+=start ) {
+		table[ii] = 999;
+	}
 
 	return 0;
 }
 
 int main(void)
 {
-	int table[10][10];
-	sievingTable(table);
-	printf("\n-------\n");
-	//tableView(table);
+	// 以下、ギブアップ。
+//	int table[10][10];
+//	sievingTable(table);
+//	table[0][0] = 999;	// falseのつもり。
+//	table[0][1] = 999;	// falseのつもり。
+//	printf("\n-------\n");
+//	//tableView(table);
+//
+//	for( int ii = 2; 10 > ii; ii++ ) {
+//		eratosthenes_sieve(table);
+//		printf("-------\n");
+//		eratosthenesSieveProcess(table, ii);
+//	}
 
-	eratosthenes_sieve();
+	int table[10*10];
+	sievingTableRe(table);
+	table[0] = 999;	// falseのつもり。
+	table[1] = 999;	// falseのつもり。
+	tableViewRe(table);
+	for( int ii = 2; 10 > ii; ii++ ) {
+		if( table[ii] == 999 ) {
+			continue;
+		}
+		printf("\n-------\n");
+		eratosthenesSieveProcessRe(table, ii);
+		eratosthenesSieveRe(table);
+	}
+
 }
 
 
