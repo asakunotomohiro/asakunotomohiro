6cd78bf58bc8dc37bc0f5de4a599cc6a76417317
プログラミングPerl言語_プロセス管理の勉強_asakunotomohiro <perlProgramming_process.Study@users.noreply.github.com>
2022-03-11 18:22:43 +0900

    Perl言語の応用知識用(プロセス管理-バッククォート)プログラム追記。

---
+++ Perl言語/応用知識用の勉強/外部コマンド実行/バッククォート.pl
@@ -0,0 +1,22 @@
+package perlStudyExecProgramming;
+$VERSION = "0.001";
+use v5.24;
+
+package backquote::sample;
+sub main() {
+	`date`;	# これだけでも標準出力はない。
+	say `date`;	# これならば、標準出力するが、改行が含まれるため、変数に一旦入れた方が良い。
+	my $date = `date`;	# 結果は、変数に代入される。
+
+	say "$date";
+			# 2022年 3月11日 金曜日 18時17分28秒 JST
+			# 	←☆改行されている。
+	chomp $date;
+	say "$date";
+			# 2022年 3月11日 金曜日 18時17分28秒 JST	←☆改行が取り除かれている。
+}
+&main();
+
+
+say "以上。"
+# vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=perl noexpandtab:
