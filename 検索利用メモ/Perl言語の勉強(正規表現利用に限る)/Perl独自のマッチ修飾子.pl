3ba7adda161f5b23412c05060acb1d60f607be97
正規表現勉強_プログラミングPerl言語編_asakunotomohiro <regularExpressions_PerlLnag.study@users.noreply.github.com>
2022-04-02 17:07:07 +0900

    正規表現利用メモ(Perl言語での正規表現メモ-マッチ修飾子)s修飾子用プログラム追記。

--- 検索利用メモ/Perl言語の勉強(正規表現利用に限る)/Perl独自のマッチ修飾子.pl
+++ 検索利用メモ/Perl言語の勉強(正規表現利用に限る)/Perl独自のマッチ修飾子.pl
@@ -7,7 +7,8 @@ my @asakuno = qw( 朝来野 智博 朝来野智博 );
 
 say "正規表現-マッチ修飾子";
 
-sub asakunoReg {
+sub asakunoRegi {
+	# マッチ修飾子-i
 	my $yesno = 'yEs';
 	if( $yesno =~ /yes/i ) {
 		# i修飾子を付けることで、パターンの大小文字を区別せずに検索するようになる。
@@ -20,7 +21,35 @@ sub asakunoReg {
 		say "i修飾子なし：検索に掛からず。";
 	}
 }
-&asakunoReg();
+#&asakunoRegi();
+
+sub asakunoRegs {
+	# マッチ修飾子-s
+	my $string = "本日は\n晴天なり。";
+	if( $string =~ /は.晴/s ) {
+		# s修飾子を付けることで、パターンの.記号を改行無視して検索するようになる。
+		say "s修飾子付き：$string";
+	}
+	if( $string =~ /は.晴/ ) {
+		say "s修飾子なし：$string";
+	}
+	else{
+		# こっちっが出力される。
+		say "s修飾子なし：検索に掛からず。";
+	}
+	if( $string =~ /\N.晴/s ) {
+		# こっちっが出力される。
+		#	s修飾子を使っている場合と思うが、改行以外にマッチさせる場合に"\N"を使う。
+		say "s修飾子あり：$string";
+	}
+	else{
+		say "s修飾子あり：検索に掛からず。";
+	}
+	unless( $string =~ /\N.晴/ ) {
+		say 's修飾子なし(\Nはs修飾子を付けた場合に機能するようだ)。';
+	}
+}
+&asakunoRegs();
 
 
 say "以上。"
