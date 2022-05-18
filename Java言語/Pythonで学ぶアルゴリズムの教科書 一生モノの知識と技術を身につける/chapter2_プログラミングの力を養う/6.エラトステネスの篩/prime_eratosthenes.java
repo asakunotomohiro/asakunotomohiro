02a23a3c34bed01a74e0612e90c2639c822b39c5
プログラミングJava言語用アルゴリズムの勉強_asakunotomohiro <java_algorithmStudy_asakunotomohiro@users.noreply.github.com>
2022-05-18 17:31:56 +0900

    Java言語での「Pythonで学ぶアルゴリズムの教科書」の「Chapter2：エラトステネスの篩」プログラム追記。
    C言語のプログラムを丸々コピペした・・・。
    無駄な作業に思えてならない・・・。
    しかし、ここまでやったのだから最後まで続けなければ、今までのがそれこそ無駄になろうて・・・。
    
    継続は力になり得るか？
    なると信じて、この章ぐらいは読み終える・・・きっと。。。
    以上。

--- Java言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/6.エラトステネスの篩/prime_eratosthenes.java
+++ Java言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/6.エラトステネスの篩/prime_eratosthenes.java
@@ -1,5 +1,5 @@
 class prime_eratosthenes {
-	static int prime_table(){
+	static int prime_table() {
 		// 表の表示。
 		for( int ii = 0, count = 10; 100 > ii; ii++ ) {
 			System.out.printf("%3d", ii);
@@ -13,8 +13,72 @@ class prime_eratosthenes {
 		return 0;
 	}
 
+	static int tablecreate(int[] table) {
+		// 篩いテーブル作成。
+		for( int ii = 0; 100 > ii; ii++ ) {
+			//table[ii] = true;
+			table[ii] = ii;
+		}
+		table[0] = 999;	// falseのつもり。
+		table[1] = 999;	// falseのつもり。
+		// 篩いテーブル内容表示。
+		for( int ii=0, jj=0; 100 > ii; ii++, jj++ ) {
+			if( jj == 10 ) {
+				System.out.printf("\n");
+				jj=0;
+			}
+			if( table[ii] == 999 ) {
+				System.out.printf("  /");
+			}
+			else{
+				System.out.printf("%3d", table[ii]);
+			}
+		}
+
+		return 0;
+	}
+
+	static int eratosthenesSieveProcess(int[] table, int start) {
+		// エラトステネスの篩を処理する関数
+		System.out.printf("%dの倍数をふるい落とす。\n", start);
+		for( int ii = start + start; 100 > ii; ii += start ) {
+			table[ii] = 999;
+		}
+
+		return 0;
+	}
+
+	static int eratosthenesSieve(int[] table) {
+		// エラトステネスの篩を表示する関数。
+		for( int ii=0, jj=0; 100 > ii; ii++, jj++ ) {
+			if( jj == 10 ) {
+				System.out.printf("\n");
+				jj=0;
+			}
+			if( table[ii] == 999 ) {
+				System.out.printf("  /");
+			}
+			else{
+				System.out.printf("%3d", table[ii]);
+			}
+		}
+
+		return 0;
+	}
+
 	public static void main(String[] args){
-		prime_table();
+//		prime_table();
+
+		int[] table = new int[10*10];	// 10x10のテーブル作成(1次元配列)。
+		tablecreate(table);	// テーブル作成後に表示する。
+		for( int ii = 2; 10 > ii; ii++ ) {
+			if( table[ii] == 999 ) {
+				continue;
+			}
+			System.out.printf("\n-------\n");
+			eratosthenesSieveProcess(table, ii);
+			eratosthenesSieve(table);
+		}
 	}
 
 }
