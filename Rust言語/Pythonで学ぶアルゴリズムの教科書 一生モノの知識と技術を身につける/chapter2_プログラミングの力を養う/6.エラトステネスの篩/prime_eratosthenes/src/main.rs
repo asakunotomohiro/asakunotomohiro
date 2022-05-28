e582fca2158e98c98967e1d7d9c58391ad96f08b
プログラミングRust言語アルゴリズムChapter2_asakunotomohiro <Rust_AlgorithmProgrammingStudy_asakunotomohiro@users.noreply.github.com>
2022-05-28 17:13:39 +0900

    Rust言語での「Pythonで学ぶアルゴリズムの教科書-Chapter2：エラトステネスの篩」表内容作成プログラム追記。
    慣れない。
    フォーマット方法が分からない。
    困った。
    スペース埋めや0埋めなど毎回調べる。
    そもそもRust言語は今後触らないように思うのだが・・・。
    
    以上。

--- Rust言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/6.エラトステネスの篩/prime_eratosthenes/src/main.rs
+++ Rust言語/Pythonで学ぶアルゴリズムの教科書 一生モノの知識と技術を身につける/chapter2_プログラミングの力を養う/6.エラトステネスの篩/prime_eratosthenes/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+fn prime_eratosthenes() {
+	let mut count = 10;
+	for ii in 0..100 {
+		print!("{0:>3}", ii);
+		count -= 1;
+		if count == 0 {
+			println!("");
+			count = 10;
+		}
+	}
 }
+
+fn main() {
+	println!("Hello, world!");
+	prime_eratosthenes();
+}
+
+/*	vim: set ts=4 sts=4 sw=4 tw=0 ff=unix fenc=utf-8 ft=rust noexpandtab: */
